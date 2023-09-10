80aa04f07bde7b955b84508db336d24e6e840a49
Carlo Eugster <carlo@relaun.ch>
2023-09-10 18:05:58 +0200

    Context menu structure tweaks

--- src/components/ContextMenu.jsx
+++ src/components/ContextMenu.jsx
@@ -1,11 +1,11 @@
-import { Fragment, useState, useRef, useEffect } from 'react'
-import { Menu, Transition } from '@headlessui/react'
+import {Fragment, useState, useRef, useEffect} from 'react'
+import {Menu, Transition} from '@headlessui/react'
 
 function classNames(...classes) {
     return classes.filter(Boolean).join(' ')
 }
 
-export default function ContextMenu({ children }) {
+export default function ContextMenu({children}) {
     const [isOpen, setIsOpen] = useState(false);
     const buttonRef = useRef(null);
     const menuRef = useRef(null);
@@ -18,18 +18,19 @@ export default function ContextMenu({ children }) {
 
     useEffect(() => {
         const handleMouseDown = (e) => {
+            console.log('handleMouseDown', e.target)
             if (
                 (buttonRef.current && buttonRef.current.contains(e.target)) ||
                 (menuRef.current && menuRef.current.contains(e.target))
             ) {
                 mouseDownInside.current = true;
-            }
-            else {
+            } else {
                 setIsOpen(false);
             }
         };
 
         const handleMouseUp = (e) => {
+            console.log('handleMouseUp', e.target)
             if (
                 mouseDownInside.current &&
                 !(buttonRef.current && buttonRef.current.contains(e.target)) &&
@@ -51,70 +52,67 @@ export default function ContextMenu({ children }) {
 
     return (
         <Menu as="div" className="relative inline-block text-left">
-            <div ref={buttonRef} onContextMenu={handleContextMenu}>
-                <Menu.Button as={Fragment}>
-                    {children}
-                </Menu.Button>
-            </div>
+            <Menu.Button ref={buttonRef} as={Fragment} onContextMenu={handleContextMenu}>
+                {children}
+            </Menu.Button>
 
-            {isOpen && (
-                <div ref={menuRef}>
-                    <Transition
-                        as={Fragment}
-                        enter="transition ease-out duration-100"
-                        enterFrom="transform opacity-0 scale-95"
-                        enterTo="transform opacity-100 scale-100"
-                        leave="transition ease-in duration-75"
-                        leaveFrom="transform opacity-100 scale-100"
-                        leaveTo="transform opacity-0 scale-95"
-                        show={isOpen}
-                    >
-                    <Menu.Items className="absolute right-0 z-10 mt-2 w-56 origin-top-right rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
-                    <div className="py-1">
-                        <Menu.Item>
-                            {({ active }) => (
-                                <a
-                                    href="#"
-                                    className={classNames(
-                                        active ? 'bg-gray-100 text-gray-900' : 'text-gray-700',
-                                        'block px-4 py-2 text-sm'
-                                    )}
-                                >
-                                    Account settings
-                                </a>
-                            )}
-                        </Menu.Item>
-                        <Menu.Item>
-                            {({ active }) => (
-                                <a
-                                    href="#"
-                                    className={classNames(
-                                        active ? 'bg-gray-100 text-gray-900' : 'text-gray-700',
-                                        'block px-4 py-2 text-sm'
-                                    )}
-                                >
-                                    Support
-                                </a>
-                            )}
-                        </Menu.Item>
-                        <Menu.Item>
-                            {({ active }) => (
-                                <a
-                                    href="#"
-                                    className={classNames(
-                                        active ? 'bg-gray-100 text-gray-900' : 'text-gray-700',
-                                        'block px-4 py-2 text-sm'
-                                    )}
-                                >
-                                    License
-                                </a>
-                            )}
-                        </Menu.Item>
-                    </div>
-                </Menu.Items>
-                    </Transition>
-                </div>
-            )}
+            <div ref={menuRef}>
+                <Transition
+                    as={Fragment}
+                    enter="transition ease-out duration-100"
+                    enterFrom="transform opacity-0 scale-95"
+                    enterTo="transform opacity-100 scale-100"
+                    leave="transition ease-in duration-75"
+                    leaveFrom="transform opacity-100 scale-100"
+                    leaveTo="transform opacity-0 scale-95"
+                    show={isOpen}
+                >
+                    <Menu.Items
+                        className="absolute left-0 z-10 w-56 origin-top-left rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
+                        <div className="py-1">
+                            <Menu.Item>
+                                {({active}) => (
+                                    <a
+                                        href="#"
+                                        className={classNames(
+                                            active ? 'bg-gray-100 text-gray-900' : 'text-gray-700',
+                                            'block px-4 py-2 text-sm'
+                                        )}
+                                    >
+                                        Account settings
+                                    </a>
+                                )}
+                            </Menu.Item>
+                            <Menu.Item>
+                                {({active}) => (
+                                    <a
+                                        href="#"
+                                        className={classNames(
+                                            active ? 'bg-gray-100 text-gray-900' : 'text-gray-700',
+                                            'block px-4 py-2 text-sm'
+                                        )}
+                                    >
+                                        Support
+                                    </a>
+                                )}
+                            </Menu.Item>
+                            <Menu.Item>
+                                {({active}) => (
+                                    <a
+                                        href="#"
+                                        className={classNames(
+                                            active ? 'bg-gray-100 text-gray-900' : 'text-gray-700',
+                                            'block px-4 py-2 text-sm'
+                                        )}
+                                    >
+                                        License
+                                    </a>
+                                )}
+                            </Menu.Item>
+                        </div>
+                    </Menu.Items>
+                </Transition>
+            </div>
         </Menu>
     )
 }
